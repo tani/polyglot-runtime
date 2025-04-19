e0d1083a58786706c1755956d885deb6f6654b7b
TANIGUCHI Masaya <ta2gch@gmail.com>
2025-04-19 23:02:56 +0900

    Remove d.ts

--- polyglot.ts
+++ polyglot.ts
@@ -2,11 +2,13 @@ import { LuaFactory } from "wasmoon";
 import * as pyodideModule from "pyodide";
 import variant from "@jitl/quickjs-wasmfile-release-sync";
 import { DefaultRubyVM } from "@ruby/wasm-wasi/dist/browser.js";
-// @deno-types="./biwascheme.d.ts"
 import BiwaScheme from "biwascheme";
 import { compileString } from "squint-cljs";
 import * as squint_core from "squint-cljs/core.js";
-import { newQuickJSWASMModuleFromVariant, type QuickJSSyncVariant } from "quickjs-emscripten-core";
+import {
+  newQuickJSWASMModuleFromVariant,
+  type QuickJSSyncVariant,
+} from "quickjs-emscripten-core";
 
 export interface Polyglot {
   call(lang: string, code: string): Promise<unknown>;
@@ -22,7 +24,9 @@ export function runClojure(polyglot: Polyglot, code: string): Promise<unknown> {
   return Promise.resolve(jsFunc(polyglot, polyglot, squint_core));
 }
 
-const QuickJS = await newQuickJSWASMModuleFromVariant(variant as unknown as QuickJSSyncVariant);
+const QuickJS = await newQuickJSWASMModuleFromVariant(
+  variant as unknown as QuickJSSyncVariant,
+);
 
 async function runJavaScript(
   _polyglot: Polyglot,
@@ -113,22 +117,46 @@ async function runRuby(_polyglot: Polyglot, code: string): Promise<unknown> {
 }
 
 function runScheme(polyglot: Polyglot, code: string): Promise<unknown> {
-  BiwaScheme.define_libfunc("polyglot-call", 2, 2, (args) => {
-    const lang = args[0] as string;
-    const snippet = args[1] as string;
-    return new BiwaScheme.Pause((pause) => {
-      polyglot.call(lang, snippet)
-        .then((res: unknown) => {
-          pause.resume(res);
-        })
-        .catch((err: unknown) => {
-          throw new BiwaScheme.Error(`Promise rejected: ${err}`);
-        });
-    });
+  BiwaScheme.define_libfunc("polyglot-call", 2, 2, (args: unknown) => {
+    if (!Array.isArray(args) || args.length !== 2) {
+      throw new BiwaScheme.Error("polyglot-call expects exactly 2 arguments");
+    }
+
+    const [lang, snippet] = args;
+
+    if (typeof lang !== "string") {
+      throw new BiwaScheme.Error(
+        "First argument to polyglot-call must be a string",
+      );
+    }
+
+    if (typeof snippet !== "string") {
+      throw new BiwaScheme.Error(
+        "Second argument to polyglot-call must be a string",
+      );
+    }
+
+    return new BiwaScheme.Pause(
+      (pause: { resume: (obj: unknown) => unknown }) => {
+        polyglot.call(lang, snippet)
+          .then((res) => {
+            pause.resume(res);
+          })
+          .catch((err) => {
+            throw new BiwaScheme.Error(`Promise rejected: ${err}`);
+          });
+      },
+    );
   });
+
   const biwa = new BiwaScheme.Interpreter((err: unknown) => {
-    throw err;
+    if (err instanceof Error) {
+      throw err;
+    } else {
+      throw new Error(String(err));
+    }
   });
+
   return new Promise((resolve, reject) =>
     biwa.evaluate(`(begin ${code})`, resolve, reject)
   );
